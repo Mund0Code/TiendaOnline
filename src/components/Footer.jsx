e33b1bc3a2dd266dd4b099038874d39862282971
Juan Pablo Patino Lopez <fomundi.fr@gmail.com>
2025-06-20 09:58:00 +0200

    feat: Actualizar enlaces en Footer y Header a MundonlineBooks

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ export default function Footer() {
       <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
         <div className="sm:flex sm:items-center sm:justify-between">
           <a
-            href="https://flowbite.com/"
+            href="https://mundonlinebooks.vercel.app"
             className="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse"
           >
             <img
@@ -42,8 +42,11 @@ export default function Footer() {
         <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
         <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">
           © 2025{" "}
-          <a href="https://flowbite.com/" className="hover:underline">
-            Flowbite™
+          <a
+            href="https://mundonlinebooks.vercel.app"
+            className="hover:underline"
+          >
+            MundonlineBooks
           </a>
           . All Rights Reserved.
         </span>
