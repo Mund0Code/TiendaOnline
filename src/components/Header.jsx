8afaaa16442d86e327cdc60ff51587fe48241f06
Juan Pablo Patino Lopez <fomundi.fr@gmail.com>
2025-06-21 22:27:38 +0200

    fix: Cambiar 'class' a 'className' en el componente Header para compatibilidad con React

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -122,13 +122,13 @@ export default function Header() {
       <div
         id="sticky-banner"
         tabindex="-1"
-        class="fixed top-0 start-0 z-50 flex justify-between w-full p-4 border-b border-gray-200 bg-gray-50 dark:bg-gray-700 dark:border-gray-600"
+        className="fixed top-0 start-0 z-50 flex justify-between w-full p-4 border-b border-gray-200 bg-gray-50 dark:bg-gray-700 dark:border-gray-600"
       >
-        <div class="flex items-center mx-auto">
-          <p class="flex items-center text-sm font-normal text-gray-500 dark:text-gray-400">
-            <span class="inline-flex p-1 me-3 bg-gray-200 rounded-full dark:bg-gray-600 w-6 h-6 items-center justify-center shrink-0">
+        <div className="flex items-center mx-auto">
+          <p className="flex items-center text-sm font-normal text-gray-500 dark:text-gray-400">
+            <span className="inline-flex p-1 me-3 bg-gray-200 rounded-full dark:bg-gray-600 w-6 h-6 items-center justify-center shrink-0">
               <svg
-                class="w-3 h-3 text-gray-500 dark:text-gray-400"
+                className="w-3 h-3 text-gray-500 dark:text-gray-400"
                 aria-hidden="true"
                 xmlns="http://www.w3.org/2000/svg"
                 fill="currentColor"
@@ -136,27 +136,27 @@ export default function Header() {
               >
                 <path d="M15 1.943v12.114a1 1 0 0 1-1.581.814L8 11V5l5.419-3.871A1 1 0 0 1 15 1.943ZM7 4H2a2 2 0 0 0-2 2v4a2 2 0 0 0 2 2v5a2 2 0 0 0 2 2h1a2 2 0 0 0 2-2V4ZM4 17v-5h1v5H4ZM16 5.183v5.634a2.984 2.984 0 0 0 0-5.634Z" />
               </svg>
-              <span class="sr-only">Light bulb</span>
+              <span className="sr-only">Light bulb</span>
             </span>
             <span>
               New brand identity has been launched for the{" "}
               <a
                 href="https://flowbite.com"
-                class="inline font-medium text-blue-600 underline dark:text-blue-500 hover:no-underline"
+                className="inline font-medium text-blue-600 underline dark:text-blue-500 hover:no-underline"
               >
                 Flowbite Library
               </a>
             </span>
           </p>
         </div>
-        <div class="flex items-center">
+        <div className="flex items-center">
           <button
             data-dismiss-target="#sticky-banner"
             type="button"
-            class="shrink-0 inline-flex justify-center w-7 h-7 items-center text-gray-400 hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 dark:hover:bg-gray-600 dark:hover:text-white"
+            className="shrink-0 inline-flex justify-center w-7 h-7 items-center text-gray-400 hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 dark:hover:bg-gray-600 dark:hover:text-white"
           >
             <svg
-              class="w-3 h-3"
+              className="w-3 h-3"
               aria-hidden="true"
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
@@ -170,7 +170,7 @@ export default function Header() {
                 d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"
               />
             </svg>
-            <span class="sr-only">Close banner</span>
+            <span className="sr-only">Close banner</span>
           </button>
         </div>
       </div>
