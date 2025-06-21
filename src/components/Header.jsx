45e1a961734394c2d3c1f9eafe5ac1fe302e76d4
Juan Pablo Patino Lopez <fomundi.fr@gmail.com>
2025-06-21 22:29:29 +0200

    fix: Eliminar la clase 'fixed' del banner para mejorar la disposición del encabezado

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -122,7 +122,7 @@ export default function Header() {
       <div
         id="sticky-banner"
         tabindex="-1"
-        className="fixed top-0 start-0 z-50 flex justify-between w-full p-4 border-b border-gray-200 bg-gray-50 dark:bg-gray-700 dark:border-gray-600"
+        className="start-0 z-50 flex justify-between w-full p-4 border-b border-gray-200 bg-gray-50 dark:bg-gray-700 dark:border-gray-600"
       >
         <div className="flex items-center mx-auto">
           <p className="flex items-center text-sm font-normal text-gray-500 dark:text-gray-400">
